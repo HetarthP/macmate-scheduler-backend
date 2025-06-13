5094debecf91226fe8e350ce7e4f2186d1642825
HetarthP <hetarthparikh@gmail.com>
2025-06-12 20:05:54 -0400

    🔧 Add CORS support for Vercel frontend

--- src/main/java/com/mcmaster/scheduler/controller/AuthController.java
+++ src/main/java/com/mcmaster/scheduler/controller/AuthController.java
@@ -7,6 +7,8 @@ import com.mcmaster.scheduler.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 import java.util.Optional;
 
